5d79cc4839cbcb4a2903d1703e0a3dd689f9eca8
Agamya Samuel <48693970+Agamya-Samuel@users.noreply.github.com>
2024-06-21 01:09:45 +0530

    privacy policy
    and other shenanigans

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,13 @@
+import { privacyPolicy } from '@/constants';
+
+export default function PrivacyPolicyPage() {
+	return (
+		<div>
+			<h1>Privacy Policy</h1>
+			<p>This is the privacy policy page for your app.</p>
+			<p>Insert your privacy policy content here.</p>
+
+			<div>{privacyPolicy}</div>
+		</div>
+	);
+}
